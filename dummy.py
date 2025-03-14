9d1537d33dec4efaf5c3ecc54c3c01f12449082f
Senthil <senthilkumarsn28@gmail.com>
2025-03-14 08:47:16 +0530

    Strings input added

--- dummy.py
+++ dummy.py
@@ -1,1 +1,11 @@
 print("Hey this is just dummy file and first change i am doing in this file")
+
+print("requirements got changed & i am going to add input variables for add two strings")
+
+a = input()
+b = input()
+
+c= a+b
+
+print("c = ", c)
+
