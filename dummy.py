38dbfaba17893557dacbb1a33b9a99bfe15b19f4
Senthil <senthilkumarsn28@gmail.com>
2025-03-26 22:33:35 +0530

    added description to input variables in soft way

--- dummy.py
+++ dummy.py
@@ -2,8 +2,8 @@ print("Hey this is just dummy file and first change i am doing in this file")
 
 print("requirements got changed & i am going to add input variables for add two strings")
 
-a = input()
-b = input()
+a = input("Your sweet first name:")
+b = input("your sweet second name:")
 
 c= a+b
 
