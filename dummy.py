ec9b8a31257f14be22c89f9ba045dc87d572294b
Senthilkumarsn28 <senthilkumarsn28@gmail.com>
2025-03-26 23:20:52 +0530

    "added description at line 4 for user convenience"

--- dummy.py
+++ dummy.py
@@ -1,6 +1,7 @@
 print("Hey this is just dummy file and first change i am doing in this file")
 
 print("requirements got changed & i am going to add input variables for add two strings")
+print("We are collecting your basic info")
 
 a = input("Your sweet first name:")
 b = input("your sweet second name:")
